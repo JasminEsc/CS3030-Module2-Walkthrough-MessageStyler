fd8a7bf82c5f2229182afecb615c38a54aa87f98
JasminEsc <jasminescandor@mail.weber.edu>
2025-09-24 14:10:21 -0600

    Task A: add -n NAME

--- style.sh
+++ style.sh
@@ -2,13 +2,6 @@
 # Name: Jasmin | CS 3030 | Module 2 (Tutorial)
 # Mini-Programming Walktrhough: Message Styler
 
-#TO-DO:
-    # Create repo to build & track features with branches
-    # this script is all of the examples throughout the last several lessons compiled into one walkthrough.
-        # Context: Message Styler - a tiny script that prints a greeting. 
-        # Goal: Practice the full Git flow: create repo, make a feature on a branch, test,  merge to main, repeat.
-        # Submission: None. This is for you to practice with branching before working with your lab.
-
 
 usage() {
   cat <<TXT
