955ad7f8636cd718e251bb6a085bede250c4f199
JasminEsc <jasminescandor@mail.weber.edu>
2025-09-24 15:21:33 -0600

    Task D: add positional custom message

--- style.sh
+++ style.sh
@@ -24,30 +24,45 @@ error() {
 
 name="stranger"     # default name
 count=1             # default count
-upper=false         # default upper; taskC added
+upper=false         # default upper
 
 # parse option (-h, -n NAME, -c COUNT, -u)
-while getopts ":hn:c:u" opt; do        # inside getopts string ( u ); taskC added
+while getopts ":hn:c:u" opt; do        # inside getopts string ( u )
   case "$opt" in
     h) usage; exit 0 ;;           # show help
     n) name="$OPTARG" ;;          # show name
     c) count="$OPTARG" ;;         # show count; taskB added
-    u) upper=true ;;              # set upper case flag; taskC added
+    u) upper=true ;;              # set upper case flag
     \?) error "Unknown option: -$OPTARG" ;;
     :) error "Missing argument for -$OPTARG" ;;
   esac
 done
+
 
 # Validate count (must be integer >= 1)
 if ! [[ "$count" =~ ^[1-9][0-9]*$ ]]; then
   error "COUNT must be a positive integer (>=1)."
 fi
 
-# Print greeting COUNT times
-msg="Hello, $name!"       # default message; taskC added
 
+# Handle positional custom message; taskD added
+shift $((OPTIND-1))
+custom="${1:-}"
+
+# Build the message ONCE (don’t overwrite it later); from taskD instructions
+# msg="${custom:-Hello, $name!}"
+
+# Build message; taskD added (chatgpt suggested; same as above, but easier to read and debug)
+if [[ -n "$custom" ]]; then
+  msg="$custom"
+else
+  msg="Hello, $name!"
+fi
+
+
+# Print greeting COUNT times
 for ((i=1; i<=count; i++)); do
-  # if true, translating characters; taskC added
+  # if true, translating characters
   if [ "$upper" = true ]; then
     echo "$msg" | tr '[:lower:]' '[:upper:]'
   else
